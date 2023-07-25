146e71272c5cef5f491a9275dc806e01435452a2
hamedsalim1999 <hamed.salimzadeh@viaplaygroup.com>
2023-07-25 13:21:32 +0200

    Start k8s part

--- Makefile
+++ Makefile
@@ -2,40 +2,11 @@ stop:
 	docker-compose -f src/docker-compose.yaml stop
 
 start:
-	docker-compose -f src/docker-compose.yaml up  -d
+	docker-compose -f src/docker-compose.yaml up  -d --no-deps --build 
 
 test:
 	docker-compose -f src/docker-compose.yaml up --build --exit-code-from test
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+build:
+	docker build -t hamedsalim1999/go-samole:$(version) src 
+	docker push hamedsalim1999/go-samole:$(version) 
--- src/main.go
+++ src/main.go
@@ -1,23 +1,28 @@
 package main
 
 import (
-        "io"
-        "log"
-		"os"
-        "net/http"
+	"io"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
+
 func main() {
-		logger := log.New(os.Stdout, "http: ", log.LstdFlags)
-		logger.Println("Server is starting...")
-        http.HandleFunc("/", Tmp)
-        err := http.ListenAndServe(":8080", nil)
-        if err != nil {
-                log.Fatal(err)
-        }
+	http.Handle("/metrics", promhttp.Handler())
+	http.ListenAndServe(":8088", nil)
+	logger := log.New(os.Stdout, "http: ", log.LstdFlags)
+	logger.Println("go-server is starting...")
+	http.HandleFunc("/", Tmp)
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
 
 func Tmp(w http.ResponseWriter, r *http.Request) {
-		logger := log.New(os.Stdout, "http: ", log.LstdFlags)
-        io.WriteString(w, "42")
-		logger.Println("Status:",200)
+	logger := log.New(os.Stdout, "http: ", log.LstdFlags)
+	io.WriteString(w, "42")
+	logger.Println("Status:", 200)
 }
