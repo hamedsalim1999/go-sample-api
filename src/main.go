dc029442e282c534b60a0adceb98cb88698084c8
hamedsalim1999 <hamed.salimzadeh@viaplaygroup.com>
2023-07-28 10:59:50 +0200

    fix port problem

--- src/main.go
+++ src/main.go
@@ -5,15 +5,10 @@ import (
 	"log"
 	"net/http"
 	"os"
-
-	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
 func main() {
-	http.Handle("/metrics", promhttp.Handler())
-	http.ListenAndServe(":8088", nil)
-	logger := log.New(os.Stdout, "http: ", log.LstdFlags)
-	logger.Println("go-server is starting...")
+
 	http.HandleFunc("/", Tmp)
 	err := http.ListenAndServe(":8080", nil)
 	if err != nil {
